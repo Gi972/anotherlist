550299ca7b4167029b6c2aebcb24dadd77ee3b29
Gi972 <jeanchristophedemar@yahoo.fr>
2020-01-15 17:01:01 +0100

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# anotherlist
-Created with CodeSandbox
---
+++ src/App.tsx
@@ -0,0 +1,33 @@
+import * as React from "react";
+import "./styles.css";
+
+///TODO
+
+// create a array of row of todo
+// create a row of todo
+// add a input for add a todo
+// display the todo
+// delete the todo
+// display totol of todo
+// filter between different status of todo
+// edit the todo
+// register and get the todo in the cloud
+// move a todo in the array
+// add a file into a todo
+// add design
+
+// very avanced program a notes with display
+// add share for  third party
+
+// inside todo
+// title and flag
+// add hour
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+    </div>
+  );
+}
